73ccd648b94967da7f186267c41b2f583ebcc8df
Orkun <orkuneyb@gmail.com>
2024-12-15 12:11:56 +0300

    add showing parent alert

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,6 +26,33 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  Future<void> showParentAlert(BuildContext context) async {
+    final prefs = await SharedPreferences.getInstance();
+    final hasShownAlert = prefs.getBool('has_shown_parent_alert') ?? false;
+
+    if (!hasShownAlert) {
+      if (context.mounted) {
+        await showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (context) => AlertDialog(
+            title: Text('parent_alert_title'.tr()),
+            content: Text('parent_alert_content'.tr()),
+            actions: [
+              TextButton(
+                child: Text('parent_alert_button'.tr()),
+                onPressed: () {
+                  Navigator.pop(context);
+                  prefs.setBool('has_shown_parent_alert', true);
+                },
+              ),
+            ],
+          ),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,8 +75,12 @@ class MyApp extends StatelessWidget {
         ),
         initialRoute: "/homePage",
         routes: {
-          '/homePage': (context) => const HomePage(),
-          // '/animalDetails': (context) => const AnimalDetailsPage(),
+          '/homePage': (context) {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              showParentAlert(context);
+            });
+            return const HomePage();
+          },
           '/settingsPage': (context) => const SettingsPage(),
         },
       ),
